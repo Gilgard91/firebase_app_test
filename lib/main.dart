ed221ffa3278b62d4023fce6b31d409ad81c32ac
giordano <giordano.zagatti@diagotlc.com>
2025-07-18 08:33:43 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
   );
   runApp(const MyApp());
 }
-//commento a caso222333444555
+//commento a caso111
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
